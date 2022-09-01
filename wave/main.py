e71561e0139eee1c02d383974f96cc056d2a53aa
jewonjewon <2jeown@kookmin.ac.kr>
2022-09-01 15:51:26 +0900

    wave v20220901

--- wave/main.py
+++ wave/main.py
@@ -1,5 +1,6 @@
 import tkinter
 import tkinter.font
+from tkinter import ttk
 from tkinter import *
 import time
 # pyinstaller -i=./wave.ico -F -w main.py     # i : 아이콘 모양, f: 하나의 파일, w : 콘솔실행없음.
@@ -8,50 +9,71 @@ import time
 
 window=tkinter.Tk()
 
-font = tkinter.font.Font(family="Consolas",size=60)
+font = tkinter.font.Font(family="Consolas",size=40)
 
 window.title("practic")
 
-window.attributes('-alpha',0.1)   # 투명도 조절. 0~1
+#window.attributes('-alpha',1)   # 투명도 조절. 0~1
+window.wm_attributes("-transparent", True)
 window.wm_attributes("-topmost", 1) # 창을 항상 상단에 배치 / 0 외 모든 인자 True
 #window size
-window.geometry("2500x1500+0+0")   #width x height + x coor + y coor
+window.geometry("800x500+20+20")   #width x height + x coor + y coor
 #window.attributes('-fullscreen',True)
 window.resizable(True, True)           # width, height   size var
 
-#label
-##textstr = "FDLFDL \n" 
-#FDL =""
-#for i in range(3):
-#    FDL += textstr
 
-#label=tkinter.Label(window, text=FDL, fg="black",font=font)
-
-#label.pack()
 
 textstr = "FDL@kookmin.ac.kr %Y-%m-%d %H:%M:%S "
 FDL =""
 for i in range(120):
     FDL += textstr
     if ((i+1)%4==0):
-        FDL+="\n"
+        FDL+="\n\n"
 
 def clock(): # 현재 시간 표시 / 반복
    live_T = time.strftime(FDL) # Real Time
    clock_width.config(text=live_T)
    clock_width.after(200, clock) # .after(지연시간{ms}, 실행함수)
 
-
-#clock_frame = Frame(window)
-#clock_frame.pack()
-
 clock_width = tkinter.Label(window, fg="black",font=font)
-clock_width.grid()
-clock()
+clock_width.configure(bg='systemTransparent')
 
-# clock_width = tkinter.Label(window, fg="black",font=font)
-# clock_width.pack()
-# clock()
+
+
+def check_data():
+    if user_id.get() == "1" and password.get() == "1":
+        window2 = Toplevel(window)
+        window2.geometry("2500x1500+0+0")   #width x height + x coor + y coor
+        
+        #window.attributes('-alpha',0.5)   # 투명도 조절. 0~1
+        window2.wm_attributes("-transparent", True)
+        window2.wm_attributes("-topmost", 1) # 창을 항상 상단에 배치 / 0 외 모든 인자 True
+        window2.configure(bg='systemTransparent')
+        #window.attributes('-fullscreen',True)
+
+        print("Logged IN Successfully")
+        clock_width.grid()
+        clock()
+
+    else:
+        print("Check your Usernam/Password")
+
+user_id, password = StringVar(), StringVar()
+id =  Label(window, text = "Username : ").grid(row = 0, column = 0, padx = 100, pady = 20)
+id_in =   Entry(window, textvariable = user_id).grid(row = 0, column = 1, padx = 100, pady = 20)
+
+pw = Label(window, text = "Password : ").grid(row = 1, column = 0, padx = 100, pady = 20)
+pw_in =  Entry(window, textvariable = password).grid(row = 1, column = 1, padx = 100, pady = 20)
+
+btn= Button(window, text = "Login", command = check_data).grid(row = 2, column = 1, padx = 10, pady = 10)
+
+
+
+
+
+
+
+
 
 
 
