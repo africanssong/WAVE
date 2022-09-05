a375cc202923ad34772621177058e7ea07007fb7
enoma422 <74201624+enoma422@users.noreply.github.com>
2022-09-05 18:23:50 +0900

    Update main.py

--- wave/main.py
+++ wave/main.py
@@ -5,8 +5,7 @@ from tkinter import *
 from tkinter import messagebox
 
 import time
-import psutil
-
+from webbrowser import get
 # pyinstaller -i=./wave.ico -F -w main.py     # i : 아이콘 모양, f: 하나의 파일, w : 콘솔실행없음.
 # pip install pillow
 # pyinstaller -i=./wave.ico -n wave -F -w main.py  wave 이름 설정.
@@ -25,29 +24,24 @@ window.geometry("800x500+20+20")  # width x height + x coor + y coor
 # window.attributes('-fullscreen',True)
 window.resizable(True, True)           # width, height   size var
 
+user_id, password = StringVar(), StringVar()
+id = Label(window, text="Username : ").grid(row=0, column=0, padx=100, pady=20)
+id_in = Entry(window, textvariable=user_id).grid(
+    row=0, column=1, padx=100, pady=20)
 
-textstr = "FDL@kookmin.ac.kr %Y-%m-%d %H:%M:%S "
-FDL = ""
-for i in range(120):
-    FDL += textstr
-    if ((i+1) % 4 == 0):
-        FDL += "\n\n"
-
-
-def clock():  # 현재 시간 표시 / 반복
-    live_T = time.strftime(FDL)  # Real Time
-    clock_width.config(text=live_T)
-    clock_width.after(200, clock)  # .after(지연시간{ms}, 실행함수)
-
+pw = Label(window, text="Password : ").grid(row=1, column=0, padx=100, pady=20)
+pw_in = Entry(window, textvariable=password).grid(
+    row=1, column=1, padx=100, pady=20)
 
 clock_width = tkinter.Label(window, fg="black", font=font)
 clock_width.configure(bg='systemTransparent')
 
-
 def check_data():
-    if user_id.get() == "1" and password.get() == "1":
+    user_id_str = user_id.get()
+    print(user_id_str)
 
-        mylist =    window.grid_slaves()
+    if user_id.get() == "1" and password.get() == "1":
+        mylist = window.grid_slaves()
         for i in mylist:
             i.destroy() 
         window.geometry("2500x1500+0+0")  # width x height + x coor + y coor
@@ -61,6 +55,9 @@ def check_data():
         print("Logged IN Successfully")
         clock_width.grid()
         clock()
+        
+        
+
 
     else:
         # 로그인 정보가 유효하지 않음을 나타내는 팝업창 띄우기
@@ -68,22 +65,50 @@ def check_data():
         print("Check your Usernam/Password")
 
 
-user_id, password = StringVar(), StringVar()
-id = Label(window, text="Username : ").grid(row=0, column=0, padx=100, pady=20)
-id_in = Entry(window, textvariable=user_id).grid(
-    row=0, column=1, padx=100, pady=20)
-
-pw = Label(window, text="Password : ").grid(row=1, column=0, padx=100, pady=20)
-pw_in = Entry(window, textvariable=password).grid(
-    row=1, column=1, padx=100, pady=20)
-
 btn = Button(window, text="Login", command=check_data).grid(
     row=2, column=1, padx=10, pady=10)
 
-###     줌 강제종료 명령어  ###
-for proc in psutil.process_iter():
-    if proc.name() == "zoom.us":
-        proc.kill()
+FDL = " @kookmin.ac.kr %Y-%m-%d %H:%M:%S   "
+
+def clock():  # 현재 시간 표시 / 반복
+    
+    live_T = time.strftime(FDL)  # Real Time
+
+    multiFDL = " "
+
+    for i in range(20):
+        multiFDL += user_id.get()+live_T
+        if ((i+1) % 4 == 0):
+            multiFDL += "\n\n"
+    
+    
+    clock_width.config(text= multiFDL)
+    clock_width.after(200, clock)  # .after(지연시간{ms}, 실행함수)
+
+
+
+
+# FDL = " FDL@kookmin.ac.kr %Y-%m-%d %H:%M:%S "
+
+
+# def clock():  # 현재 시간 표시 / 반복
+    
+#     live_T = time.strftime(FDL)  # Real Time
+
+#     multiFDL = ""
+#     for i in range(20):
+#         multiFDL += user_id.get()+FDL
+#         if ((i+1) % 4 == 0):
+#             multiFDL += "\n\n"
+
+#     clock_width.config(text= multiFDL)
+#     clock_width.after(200, clock)  # .after(지연시간{ms}, 실행함수)
+
+
+
+
+
+
 
 # #button
 # count=0
